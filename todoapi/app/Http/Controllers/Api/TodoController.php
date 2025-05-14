315e546d575bec694395e77de06887c26e3f2e98
luiz-hms <luiz.henriquems@hotmail.com>
2025-05-13 21:09:08 -0300

    listagem de todas as todos

--- todoapi/app/Http/Controllers/Api/TodoController.php
+++ todoapi/app/Http/Controllers/Api/TodoController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Todo;
+use App\Http\Resources\TodoResource;
+
 class TodoController extends Controller
 {
+    public function __construct(protected Todo $todoRepository){}
     public function index(){
-        return Todo::all();
+        $todos = $this->todoRepository->all();
+        return TodoResource::collection($todos);
     }
 }
--- todoapi/app/Models/Todo.php
+++ todoapi/app/Models/Todo.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+    //use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'title',
+        'user_id',
+        'created_at',
+    ];
 }
